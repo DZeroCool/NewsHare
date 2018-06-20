e35da3e0b0eb5c5f0a0d490fe6063d3f7ba3d437
Doug <28231137+DZeroCool@users.noreply.github.com>
2018-06-20 12:39:20 -0700

    updated software for main program

--- rabbit.py
+++ rabbit.py
@@ -1,12 +1,110 @@
 # -*- coding: utf-8 -*-
 import re, feedparser, time
-from rabbit_content import *
 from urllib.parse import urlparse
 
 DEVELOPMENT_VERSION = 'beta'
-DEPLOYMENT_ACTIVE = False
-APACHE_DIR = '/var/www/html' if DEPLOYMENT_ACTIVE else ''
-RABBIT_DIR = '/home/supremeleader/' if DEPLOYMENT_ACTIVE else ''
+
+feeds =  """http://feeds.bbci.co.uk/news/rss.xml
+https://www.wired.com/feed/rss
+https://www.theguardian.com/world/rss
+https://www.theguardian.com/uk/rss
+https://www.newyorker.com/feed/everything
+https://www.politico.com/rss/politics08.xml
+https://www.politico.com/rss/economy.xml
+http://feeds.reuters.com/reuters/businessNews
+http://feeds.reuters.com/news/wealth
+http://feeds.reuters.com/Reuters/PoliticsNews
+http://feeds.reuters.com/reuters/scienceNews
+http://feeds.reuters.com/reuters/technologyNews
+http://feeds.reuters.com/Reuters/worldNews
+http://feeds.washingtonpost.com/rss/politics
+http://feeds.washingtonpost.com/rss/world
+http://feeds.washingtonpost.com/rss/lifestyle
+http://www.wsj.com/xml/rss/3_7085.xml
+http://www.wsj.com/xml/rss/3_7031.xml"""
+
+categories, txt_categories, dict_categories = [], [], {}
+
+def addCategory(name, criteria):
+    global categories, txt_categories, dict_categories
+    categories.append(criteria); txt_categories.append(name)
+    dict_categories[name] = criteria
+
+tech = """
+20: feds 
+25: trade
+10: development
+10: whatsapp
+50: genetic 
+50: genes 
+
+25: google
+10: amazon
+5: uber
+10: microsoft
+10: tesla
+15: musk
+10: Apple 
+
+5: startup
+5: start-up
+10: seattle
+10: california
+"""
+addCategory('Technology', tech)
+
+crypto = """
+20: crypto
+10: bitcoin
+15: altcoins
+15: alt coins"""
+addCategory('Cryptography', crypto)
+
+money = """
+25: money
+20: economy
+25: stock-market
+25: stockmarket
+10: wealth
+25: savings
+40: gold"""
+addCategory('Money', money)
+
+space_stuff = """
+65: NASA
+25: astronomy
+25: MIT
+500: faster than light
+500: faster-than-light
+500: wormholes
+500: space travel
+250: space colonization
+90: space, space travel, faster then light travel, mars colonization, aliens, wormholes #wut?
+20: planets
+"""
+addCategory('Space', space_stuff)
+
+climate = """
+15: climate change
+15: carbon
+40: air pollution, toxic
+"""
+addCategory('Climate', climate)
+
+world_affairs = """
+10: nuclear
+15: energy
+25: trump
+20: korea
+5: pompeo
+20: Russia
+20: Italy 
+15: trade, trump
+35: sanctions
+"""
+addCategory('World Affairs', world_affairs)
+
+matches, named_categories, qualifications = [], list(dict_categories.keys()), {}
 
 
 def isMatched(entry,criteria):
@@ -122,10 +220,113 @@ def generate_results():
         output += 'Filter efficiency %.3f (%d matches/%d results)' % (100*efficiency, len(list_matches), resultCount) + '<br>'
     return output
 
+template = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<style>
+body {
+  margin: 0;
+  font-size: 28px;
+}
 
-templateFile = open(RABBIT_DIR + 'template.html', 'r', encoding="utf-8")
-template = templateFile.read()
-templateFile.close()
+.header {
+  background-color: #f1f1f1;
+  padding: 30px;
+  text-align: center;
+}
+
+#navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+#navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+#navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+#navbar a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.content {
+  padding: 16px;
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.sticky + .content {
+  padding-top: 60px;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h2>News Hare</h2>
+  <p>Your News Fast</p>
+</div>
+
+<div id="navbar">
+  <a class="active" href="__gohome__">Home</a>
+  <a href="javascript:void(0)">News</a>
+  <a href="javascript:void(0)">Contact</a>
+</div>
+
+<div class="content">
+ 
+</div>
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+    <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>rssRabbit __development_version__</title>
+</head>
+<body>
+<big><big><big><span
+ style="color: rgb(15, 24, 253);">rss</span>Rabbit</big></big></big>
+__development_version__<br>
+<br>
+Categories:<br>
+__categories__<br>
+<br>
+Date/Time of Last Update: __datetime__ UTC<br>
+<br>
+__output__<br>
+<br>
+</body>
+</html>
+"""
 
 index_html = template
 index_html = index_html.replace('__development_version__', DEVELOPMENT_VERSION)
@@ -134,6 +335,6 @@ index_html = index_html.replace('__datetime__', time.asctime())
 index_html = index_html.replace('__categories__', '<br>'.join([html_link(text.strip(), 'index.html#' + ''.join(text.split()).strip()) for text in txt_categories]))
 index_html = index_html.replace('__output__', generate_results())
 
-fileOutput = open(APACHE_DIR + 'index.html', 'w', encoding='utf-16')
+fileOutput = open('index.html', 'w', encoding='utf-16')
 fileOutput.write(index_html)
 fileOutput.close()
