357499fa78e20fd020c3dbd4fb54443a68f744c1
Doug <28231137+DZeroCool@users.noreply.github.com>
2018-06-20 13:09:06 -0700

    updated

--- rabbit.py
+++ rabbit.py
@@ -1,111 +1,22 @@
 # -*- coding: utf-8 -*-
-import re, feedparser, time
+import re, feedparser, time, datetime
+from rabbit_content import *
 from urllib.parse import urlparse
 
 DEVELOPMENT_VERSION = 'beta'
+DEPLOYMENT_ACTIVE = True
+APACHE_DIR = '/var/www/html/' if DEPLOYMENT_ACTIVE else ''
+RABBIT_DIR = '/home/supremeleader/' if DEPLOYMENT_ACTIVE else ''
+DEGRADATION_DATE, EXPIRATION_DATE = 2, 9
+now = datetime.datetime.now()
 
-feeds =  """http://feeds.bbci.co.uk/news/rss.xml
-https://www.wired.com/feed/rss
-https://www.theguardian.com/world/rss
-https://www.theguardian.com/uk/rss
-https://www.newyorker.com/feed/everything
-https://www.politico.com/rss/politics08.xml
-https://www.politico.com/rss/economy.xml
-http://feeds.reuters.com/reuters/businessNews
-http://feeds.reuters.com/news/wealth
-http://feeds.reuters.com/Reuters/PoliticsNews
-http://feeds.reuters.com/reuters/scienceNews
-http://feeds.reuters.com/reuters/technologyNews
-http://feeds.reuters.com/Reuters/worldNews
-http://feeds.washingtonpost.com/rss/politics
-http://feeds.washingtonpost.com/rss/world
-http://feeds.washingtonpost.com/rss/lifestyle
-http://www.wsj.com/xml/rss/3_7085.xml
-http://www.wsj.com/xml/rss/3_7031.xml"""
-
-categories, txt_categories, dict_categories = [], [], {}
-
-def addCategory(name, criteria):
-    global categories, txt_categories, dict_categories
-    categories.append(criteria); txt_categories.append(name)
-    dict_categories[name] = criteria
-
-tech = """
-20: feds 
-25: trade
-10: development
-10: whatsapp
-50: genetic 
-50: genes 
-
-25: google
-10: amazon
-5: uber
-10: microsoft
-10: tesla
-15: musk
-10: Apple 
-
-5: startup
-5: start-up
-10: seattle
-10: california
-"""
-addCategory('Technology', tech)
-
-crypto = """
-20: crypto
-10: bitcoin
-15: altcoins
-15: alt coins"""
-addCategory('Cryptography', crypto)
-
-money = """
-25: money
-20: economy
-25: stock-market
-25: stockmarket
-10: wealth
-25: savings
-40: gold"""
-addCategory('Money', money)
-
-space_stuff = """
-65: NASA
-25: astronomy
-25: MIT
-500: faster than light
-500: faster-than-light
-500: wormholes
-500: space travel
-250: space colonization
-90: space, space travel, faster then light travel, mars colonization, aliens, wormholes #wut?
-20: planets
-"""
-addCategory('Space', space_stuff)
-
-climate = """
-15: climate change
-15: carbon
-40: air pollution, toxic
-"""
-addCategory('Climate', climate)
-
-world_affairs = """
-10: nuclear
-15: energy
-25: trump
-20: korea
-5: pompeo
-20: Russia
-20: Italy 
-15: trade, trump
-35: sanctions
-"""
-addCategory('World Affairs', world_affairs)
-
-matches, named_categories, qualifications = [], list(dict_categories.keys()), {}
-
+def getAgeInDays(entry):
+    try:
+        then = datetime.datetime(*entry['published_parsed'][:7])
+        delta = now - then
+        return max(0, delta.days)
+    except:
+        return None
 
 def isMatched(entry,criteria):
     str_entry = str(entry).lower()
@@ -131,6 +42,11 @@ def qualification(entry,criteria):
     global qualifications
     title = entry['title']
     qualifications[title] = []
+    age = getAgeInDays(entry)
+    scalar = 1
+    EXPIRATION_RANGE = EXPIRATION_DATE - DEGRADATION_DATE
+    if age is not None and age >= DEGRADATION_DATE:
+        scalar = (EXPIRATION_RANGE - min(EXPIRATION_RANGE - 1, age - DEGRADATION_DATE)) / EXPIRATION_RANGE
     score = 0
     scoring_weights = {}
     for criterion in criteria.split('\n'):
@@ -139,7 +55,7 @@ def qualification(entry,criteria):
         try:
             weight = float(criterion.split(":")[0].strip())
             target = criterion[criterion.find(':')+1:]
-            scoring_weights[target] = weight
+            scoring_weights[target] = weight*scalar
         except:
             pass
     weighted_criteria = []
@@ -158,7 +74,7 @@ def qualification(entry,criteria):
             if foundAll:
                 score = score + weight
                 qualifications[title].append((weight, token))
-        except: 
+        except:
             pass
     return score
 
@@ -167,17 +83,20 @@ def html_link(text, address):
 
 def generate_results():
     global qualifications
-    entries = []
+    entries, setTitles = [], set()
     resultCount = 0
     for feed in feeds.split('\n'):
         feed = feed.strip()
         if len(feed) > 1:
            # print('Current feed:', feed)
             d = feedparser.parse(feed)
-            resultCount += len(d['entries'])
+##            resultCount += len(d['entries'])
 ##            print('Entries present:', len(d['entries']))
             for entry in d['entries']:
-                entries.append(entry)
+                if entry['title'] not in setTitles:
+                    entries.append(entry)
+                    setTitles.add(entry['title'])
+                    resultCount += 1
     output = ''
     for named_category in txt_categories:
         category = dict_categories[named_category]
@@ -211,8 +130,11 @@ def generate_results():
                 output += match['description'] + '<br>'
             except:
                 pass
-            output += 'Match ID: ' + str(matchID) + ' Score: ' + str(weighted_match[0]) + ' source: '+ urlparse(match['link']).netloc + '<br>'
-            output += 'qualifiers: ' + ', '.join(['%d %s' % (qualifier[0], qualifier[1]) for qualifier in qualifiers]) + '<br>'
+            output += 'Match ID: ' + str(matchID) + ' Score: %.2f' % weighted_match[0] + ' source: '+ urlparse(match['link']).netloc
+            age = getAgeInDays(match)
+            if age is not None:
+                output += ' age: %d days' % age if age is not 1 else ' age: 1 day'
+            output += '<br>qualifiers: ' + ', '.join(['%.2f %s' % (qualifier[0], qualifier[1]) for qualifier in qualifiers]) + '<br>'
             output += '<br>'
             matchID += 1
 
@@ -220,113 +142,10 @@ def generate_results():
         output += 'Filter efficiency %.3f (%d matches/%d results)' % (100*efficiency, len(list_matches), resultCount) + '<br>'
     return output
 
-template = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<style>
-body {
-  margin: 0;
-  font-size: 28px;
-}
 
-.header {
-  background-color: #f1f1f1;
-  padding: 30px;
-  text-align: center;
-}
-
-#navbar {
-  overflow: hidden;
-  background-color: #333;
-}
-
-#navbar a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-#navbar a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-#navbar a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-
-.content {
-  padding: 16px;
-}
-
-.sticky {
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-
-.sticky + .content {
-  padding-top: 60px;
-}
-</style>
-</head>
-<body>
-
-<div class="header">
-  <h2>News Hare</h2>
-  <p>Your News Fast</p>
-</div>
-
-<div id="navbar">
-  <a class="active" href="__gohome__">Home</a>
-  <a href="javascript:void(0)">News</a>
-  <a href="javascript:void(0)">Contact</a>
-</div>
-
-<div class="content">
- 
-</div>
-
-<script>
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-</script>
-
-    <meta content="text/html; charset=ISO-8859-1"
- http-equiv="content-type">
-  <title>rssRabbit __development_version__</title>
-</head>
-<body>
-<big><big><big><span
- style="color: rgb(15, 24, 253);">rss</span>Rabbit</big></big></big>
-__development_version__<br>
-<br>
-Categories:<br>
-__categories__<br>
-<br>
-Date/Time of Last Update: __datetime__ UTC<br>
-<br>
-__output__<br>
-<br>
-</body>
-</html>
-"""
+templateFile = open(RABBIT_DIR + 'template.html', 'r', encoding="utf-8")
+template = templateFile.read()
+templateFile.close()
 
 index_html = template
 index_html = index_html.replace('__development_version__', DEVELOPMENT_VERSION)
@@ -335,6 +154,6 @@ index_html = index_html.replace('__datetime__', time.asctime())
 index_html = index_html.replace('__categories__', '<br>'.join([html_link(text.strip(), 'index.html#' + ''.join(text.split()).strip()) for text in txt_categories]))
 index_html = index_html.replace('__output__', generate_results())
 
-fileOutput = open('index.html', 'w', encoding='utf-16')
+fileOutput = open(APACHE_DIR + 'index.html', 'w', encoding='utf-16')
 fileOutput.write(index_html)
 fileOutput.close()
